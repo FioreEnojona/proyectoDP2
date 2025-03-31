50c1a20434850ac4268420d9de34ae88ab82a936
josselinPineda <149841107+josselinPineda@users.noreply.github.com>
2025-03-31 09:02:51 -0600

    parte1 de galeria

---
+++ variables.js
@@ -0,0 +1,87 @@
+const handlerBarsContext = {
+    "/index.html": {
+      
+      "galeria": [
+        {
+          src: "imagen/66528763272b1.jpeg",
+          alt: "Rescate urgente",
+          titulo: "Rescate en emergencia",
+          descripcion: "Voluntarios salvan animales en situaciones críticas durante desastres."
+        },
+        {
+          src: "imagen/cosas-donar-refugio-animales-portada-1280x720x80xX.jpg",
+          alt: "Donaciones al refugio",
+          titulo: "Ayuda solidaria",
+          descripcion: "Apoyando con alimentos, medicinas y amor para nuestros rescatados."
+        },
+        {
+          src: "imagen/HLD6ZXWZHREUZNDTKITVWRREX4.jpg",
+          alt: "Cachorros y gatitos",
+          titulo: "Nuevas oportunidades",
+          descripcion: "Animales rescatados listos para encontrar un hogar lleno de amor."
+        },
+        {
+          src: "imagen/np_equipo_rescate000.jpg",
+          alt: "Equipo de rescate",
+          titulo: "Fuerza voluntaria",
+          descripcion: "Nuestro equipo, el alma detrás de cada rescate exitoso."
+        },
+        {
+          src: "imagen/rescate-animales-medicina-veterinaria-USS.jpg",
+          alt: "Atención veterinaria",
+          titulo: "Cuidado profesional",
+          descripcion: "Veterinarios brindando atención médica urgente a los rescatados."
+        },
+        {
+          src: "imagen/resguardo-animal-CORTESIA_opt.jpg",
+          alt: "Protección animal",
+          titulo: "Refugio seguro",
+          descripcion: "Un espacio cálido para animales que escaparon del abandono."
+        },
+        {
+          src: "imagen/animales-rescatados.jpg",
+          alt: "Animales salvados",
+          titulo: "Historias de esperanza",
+          descripcion: "Cada uno de ellos, una historia de lucha y recuperación."
+        },
+        {
+          src: "imagen/foto3-naciional.jpg",
+          alt: "Curación en proceso",
+          titulo: "Cicatrizando heridas",
+          descripcion: "Animales reciben cuidados después de un rescate doloroso."
+        },
+        {
+          src: "imagen/Mas-de-300-animales-rescatados-durante-incendios-en-California..jpg",
+          alt: "Incendios y rescates",
+          titulo: "Contra el fuego",
+          descripcion: "Más de 300 animales fueron rescatados durante incendios forestales."
+        },
+        {
+          src: "imagen/Serfor-2.jpg",
+          alt: "Protección forestal",
+          titulo: "Rescate en zonas verdes",
+          descripcion: "Autoridades resguardan animales silvestres encontrados en peligro."
+        },
+        {
+          src: "imagen/FKULXJ7CLFE6JJG5P6N5LEAEFI.jpg",
+          alt: "Alimentación especial",
+          titulo: "Un nuevo comienzo",
+          descripcion: "Bebés animales reciben cuidado intensivo para sobrevivir."
+        },
+        {
+          src: "imagen/20230724_214303846_ios_0.jpg",
+          alt: "Liberación de aves",
+          titulo: "Regreso al hogar",
+          descripcion: "Después de su recuperación, estas aves vuelan libres otra vez."
+        }
+          
+      ]
+    }
+  };
+  
+  const pageContext = (page) => {
+    const context = { ...handlerBarsContext[page] };
+    return context;
+  };
+  
+  export default pageContext;
--- vite.config.js
+++ vite.config.js
@@ -1,40 +1,33 @@
-import path, { resolve } from "node:path";
 import { defineConfig } from 'vite';
 import * as glob from 'glob';
+import path, { resolve } from 'node:path';
+import { ViteMinifyPlugin } from 'vite-plugin-minify';
 import htmlPurge from 'vite-plugin-purgecss';
 import handlebars from 'vite-plugin-handlebars';
 
-const obtenerEntradasHTML = ()=>{
-    return Object.fromEntries(
-        [
-            ...glob
-                .sync('./**/*.html', {ignore: ["./dist/**", "./node_modules/**"]}
-
-            ).map (
-                fileData => [
-                    fileData.slice(0, fileData.length - path.extname(fileData).length),
-                    resolve(__dirname, fileData)
-                ]
-            )
-        ]
-    );
-}
-
+import handlerBarsContext from './variables.js';
 
 export default defineConfig({
+    base: "/galeria/",
     appType: 'mpa',
-    base: process.env.DEPLOY_BASE_URL,
     build: {
         rollupOptions: {
-            input: obtenerEntradasHTML()
-        }
+            input: Object.fromEntries(
+                [...glob.sync('./!(dist)/*.html').map(file => [
+                    file.slice(0, file.length - path.extname(file).length), resolve(__dirname, file)
+                ]),
+                ...glob.sync('./*.html').map(file => [
+                    file.slice(0, file.length - path.extname(file).length), resolve(__dirname, file)
+                ])]
+            ),
+        },
     },
     plugins: [
-
         handlebars({
-            partialDirectory: resolve(__dirname, 'partials')
+            partialDirectory: resolve(__dirname, 'partials'),
+            context: handlerBarsContext,
         }),
-        htmlPurge({})
-    ]
-
+        htmlPurge({}),
+        ViteMinifyPlugin({}),
+]
 });
