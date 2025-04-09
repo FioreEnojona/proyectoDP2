767819b7cef19acac6dde44fdafe3067dfc6bc03
FioreEnojona <estoyaquiporobligacion@gmail.com>
2025-04-08 21:52:12 -0600

    carrusel a ver

--- data/carruselprogramas.js
+++ data/carruselprogramas.js
@@ -6,21 +6,28 @@ let imagenes = [
 
 let atras = document.getElementById('atras');
 let adelante = document.getElementById('adelante');
-let imagen = document.querySelector("#img img"); 
+let imagen = document.getElementById('img');
 
 let actual = 0;
 
 function actualizarCarrusel() {
-    imagen.src = imagenes[actual].url; 
+    imagen.src = imagenes[actual].url;
+    console.log("Mostrando imagen:", imagenes[actual].url);
 }
 
+adelante.addEventListener('click', () => {
+    actual = (actual + 1) % imagenes.length;
+    actualizarCarrusel();
+});
 
+atras.addEventListener('click', () => {
+    actual = (actual - 1 + imagenes.length) % imagenes.length;
+    actualizarCarrusel();
+});
 
-
-setInterval(function() {
+setInterval(() => {
     actual = (actual + 1) % imagenes.length;
     actualizarCarrusel();
 }, 7000);
-
 
 actualizarCarrusel();
