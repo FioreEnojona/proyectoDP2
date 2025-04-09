b007c5df70132730d6a35ddd387cf9694c14e4f0
FioreEnojona <estoyaquiporobligacion@gmail.com>
2025-04-08 20:34:12 -0600

    otra vez imagenes

--- data/pages/donaciones.js
+++ data/pages/donaciones.js
@@ -2,7 +2,7 @@ export const donaciones = {
     donaciones: [
         {
             metodo: "PayPal",
-            imagen: "/imagenes/paypal.png",
+            imagen: "imagenes/paypal.png",
             alt: "fotopaypal",
             mensaje: "El siguiente botón te redirigirá a un formulario de donaciones",
             enlace: "https://www.paypal.com/paypalme/animalandia377"
