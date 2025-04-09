1d396f120688564d518f0fe2dda77ae6cd422455
FioreEnojona <estoyaquiporobligacion@gmail.com>
2025-04-08 20:18:01 -0600

    vite no se que paso

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ const obtenerEntradasHTML = ()=>{
     return Object.fromEntries(
         [
             ...glob
-                .sync('./*/.html', {ignore: ["./dist/*", "./node_modules/*"]}
+                .sync('./**/*.html', {ignore: ["./dist/**", "./node_modules/**"]}
 
             ).map (
                 fileData => [
@@ -35,7 +35,6 @@ export default defineConfig({
         handlebars({
             partialDirectory: resolve(__dirname, 'partials'),
             context: getPageContext
-
         }),
         htmlPurge({})
     ]
